6fc03ff6fce28d319068e193a57f5f3624085c9b
Karina Shupik <77788896h@gmail.com>
2024-06-27 21:26:28 +0300

    feat: password checker component

--- password_checker/src/app/password-checker/password-checker.component.ts
+++ password_checker/src/app/password-checker/password-checker.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './password-checker.component.css'
 })
 export class PasswordCheckerComponent {
+  password: string = '';
+  isEmpty: boolean = true;
+  isWeak: boolean = false;
+  isMedium: boolean = false;
+  isStrong: boolean = false;
 
+  checkStrength() {
+    this.isEmpty = this.password.length === 0;
+    this.isWeak = false;
+    this.isMedium = false;
+    this.isStrong = false;
+
+    if (this.password.length < 8) {
+      return;
+    }
+
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasDigits = /\d/.test(this.password);
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+
+    if (hasLetters && hasDigits && hasSymbols) {
+      this.isStrong = true;
+    } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+      this.isMedium = true;
+    } else {
+      this.isWeak = true;
+    }
+  }
 }
